25788645967906ae098e813ba8dc7676e0584423
icaromanfrinni <icarosousa@gmail.com>
2020-04-23 22:40:46 -0300

    Primitive Circle

--- src/RenderEngine/RenderAPI.h
+++ src/RenderEngine/RenderAPI.h
@@ -12,10 +12,11 @@
 #include <glm/gtc/type_ptr.hpp>
 #define STB_IMAGE_IMPLEMENTATION
 
+#include "Skybox.h"
 #include "Shader.h"
 #include "Camera.h"
 #include "Mesh.h"
-#include "Skybox.h"
+#include "Primitives.h"
 
 namespace CRAB
 {
@@ -127,28 +128,9 @@ namespace CRAB
 
         // load curves
         // -----------
-
-        // control points
-        std::vector<CRAB::Vector4Df> circle_points;
-	    circle_points.push_back(CRAB::Vector4Df{ 0.0f, 0.0f, 0.0f, 1.0f });
-        circle_points.push_back(CRAB::Vector4Df{ 10.0f, 0.0f, 0.0f, 1.0f });
-        circle_points.push_back(CRAB::Vector4Df{ 10.0f, 10.0f, 0.0f, 1.0f });
-        circle_points.push_back(CRAB::Vector4Df{ 10.0f, 20.0f, 0.0f, 1.0f });
-        circle_points.push_back(CRAB::Vector4Df{ 0.0f, 20.0f, 0.0f, 1.0f });
-        circle_points.push_back(CRAB::Vector4Df{ -10.0f, 20.0f, 0.0f, 1.0f });
-        circle_points.push_back(CRAB::Vector4Df{ -10.0f, 10.0f, 0.0f, 1.0f });
-        circle_points.push_back(CRAB::Vector4Df{ -10.0f, 0.0f, 0.0f, 1.0f });
-        circle_points.push_back(CRAB::Vector4Df{ 0.0f, 0.0f, 0.0f, 1.0f });
-
-        // weights
-        float w = exp2f(0.5f) / 2.0f;
-        std::vector<float> circle_weights = { 1.0f, w, 1.0f, w, 1.0f, w, 1.0f, w, 1.0f };
-
-        // knot vector
-        std::vector<float> circle_knot = { 0.0f, 0.0f, 0.0f, 0.25f, 0.25f, 0.5f, 0.5f, 0.75f, 0.75f, 1.0f, 1.0f, 1.0f };
-
-        ourMesh_List.push_back(Mesh(NURBS(circle_points, 2, circle_weights, circle_knot)));
-        ourMesh_List.push_back(Mesh(circle_points));
+        NURBS circle1 = Primitive::Circle(5.0f);
+        ourMesh_List.push_back(Mesh(circle1));
+        ourMesh_List.push_back(Mesh(circle1.points));
 
         // pass projection matrix to shader (as projection matrix rarely changes there's no need to do this per frame)
         // -----------------------------------------------------------------------------------------------------------
